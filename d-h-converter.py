9f5cfa92e51a8e3c658c0fb9fadcb686578fcf89
oljik <oljaktl@gmail.com>
2021-03-25 09:34:45 +0600

    pre 02

--- d-h-converter.py
+++ d-h-converter.py
@@ -1,7 +1,10 @@
 import wfdb
+import sys
+import numpy
+# numpy.set_printoptions(threshold=1500)
 
 record = wfdb.rdsamp('./zapisi/x34') # В папке "zapisi" лежат 2 файла "aami3a.dat" и "aami3a.hea". Если одного нет, выполнение метода даст ошибку.
 points = record[0] # Точки сигнала
 properties = record[1] # Информация о сигнале (частота дискредитизации, длина сигнала, единица измерения)
 
-print(points)
+print(points.tolist())
